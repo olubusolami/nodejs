96df6117140fde95a5fe2da265779db01e1436d8
Olúbùsọ́lá <69323769+olubusolami@users.noreply.github.com>
2021-06-01 16:31:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts')
+
+dotenv.config();
+
+//connect to mongoose
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(res => console.log('connected to database'))
+
+//middleware
+app.use(express.json());
+//Route middlewares
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(4000, () => console.log('server up and running'));
